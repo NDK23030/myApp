8bbba86aa6ead02dbc68153f3023d268b994f439
Denis Mazanov <ggmazanovdenis@gmail.com>
2023-07-13 19:56:22 +0300

    work with API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MyModal from "./components/UI/modal/MyModal";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 
+import { usePosts } from "./components/hooks/usePosts";
+
+import PostService from "./API/PostService";
+
 import './styles/App.css';
 import MyButton from "./components/UI/button/MyButton";
-import { usePosts } from "./components/hooks/usePosts";
+import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./components/hooks/useFetching";
 
 function App() {
 	const [posts, setPosts] = useState([]);
@@ -15,6 +20,14 @@ function App() {
 	const [modal, setModal] = useState(false);
 
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+		const posts = await PostService.getAll();
+		setPosts(posts);
+	})
+
+	useEffect(() => {
+		fetchPosts();
+	}, [])
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -27,6 +40,7 @@ function App() {
 
 	return (
 		<div className="App">
+			<MyButton onClick={fetchPosts}>Нажми меня</MyButton>
 			<MyButton 
 				style={{marginTop: '30px'}} 
 				onClick={() => setModal(true)}>
@@ -40,8 +54,13 @@ function App() {
 				filter={filter}
 				setFilter={setFilter}
 			/>
-			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов"/>
-
+			{postError &&
+				<h1>Произошла ошибка ${postError}</h1>
+			} 
+			{isPostsLoading
+				? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div> 
+				: <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов"/>
+			}
 		</div>
 	);
 }
